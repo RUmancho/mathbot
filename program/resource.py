6a94989e1afb3266a5d56c3a63b6f7f1e12d0e12
Your Name <you@example.com>
2025-07-31 00:50:02 +0500

    debug

--- program/resource.py
+++ program/resource.py
@@ -3,6 +3,30 @@ import json
 import os
 colorama.init()
 
+class FilePath:
+    def __init__(self, path: str):
+        self.path = path
+
+        if "\\" in self.path:
+            self.path = self.path.replace("\\", "/")
+
+    def back(self):
+        self.path = os.path.dirname(self.path)
+        return self.path
+    
+    def enter(self, directory: str):
+        self.path += f"/{directory}"
+        return self.path
+
+    def __str__(self):
+        return self.path
+
+    def __repr__(self):
+        return self.path
+
+run_file_directory = FilePath(os.getcwd())
+root_folder = run_file_directory.back()
+
 resource = {}
 
 with open("resource.json", "r", encoding="utf-8") as file:
@@ -13,9 +37,9 @@ if resource == {}:
     exit()
 
 def file_availability(relative_path: str) -> bool:
-  """ Проверяет существование файла по относительному пути."""
-  full_path = os.path.abspath(relative_path)
-  return os.path.isfile(full_path)
+    """ Проверяет существование файла по относительному пути."""
+    full_path = f"{root_folder}/{relative_path}"
+    return os.path.isfile(full_path)
 
 def get_values_from_json(json_data):
     """Функция для получения всех значений из JSON-данных"""
@@ -33,10 +57,10 @@ def get_values_from_json(json_data):
     return values
 
 
-paths = get_values_from_json(resource)
+relatives_paths = get_values_from_json(resource)
 error = False
 
-for path in paths:
+for path in relatives_paths:
     if not file_availability(path):
         print(f"{colorama.Fore.RED}File path not found:  {path}")
         error = True

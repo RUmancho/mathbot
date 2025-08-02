17a5e1b980958d543efb025ab35c900c16648e1d
Your Name <you@example.com>
2025-08-02 16:55:58 +0500

    add bot username

--- program/config.py
+++ program/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 LLM_API_KEY = os.getenv("LLM_API")
 
+USERNAME = "@math_v1bot"
 DEFAULT_LANGUAGE = "ru"
 SUPPORT_MAIL = "mathbothelp@gmail.com"
 SUPPORT_LIMIT_DAY = 5

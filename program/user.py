4860c9a31e843c82f9a56063352dea054c8738ff
Your Name <you@example.com>
2025-08-08 12:40:32 +0500

    processes added

--- program/user.py
+++ program/user.py
@@ -7,6 +7,7 @@
 """
 
 from database import Manager, Tables
+import config
 from core import Button, cancelable
 from sqlalchemy import and_
 import buttons as btn
@@ -531,7 +532,164 @@ class Unregistered(User):
 
     def __init__(self, myID: str = "", bind_bot = None):
         super().__init__(myID, bind_bot)
-        self.current_registration = None
+        self.current_registration = None  # совместимость со старой логикой
+        self.reg_process = None          # новый процесс регистрации (Process)
+        self.in_registration = False
+
+    class _BaseRegistration(core.Process):
+        """Базовый класс для регистрационных процессов."""
+        def __init__(self, ID):
+            super().__init__(ID, cancelable=True)
+            self._data: dict = {}
+            self.completed: bool = False
+
+        def _store(self, key: str, value):
+            self._data[key] = value
+
+        def _send(self, text: str):
+            self._bot.send_message(self._me.get_ID(), text)
+
+    class TeacherRegistration(_BaseRegistration):
+        """Пошаговая регистрация учителя: имя → фамилия → пароль → сохранение."""
+        def __init__(self, ID):
+            super().__init__(ID)
+            self._chain = [
+                self.ask_name, self.verify_name,
+                self.ask_surname, self.verify_surname,
+                self.ask_password, self.verify_password,
+            ]
+            self._max_i = len(self._chain) - 1
+
+        def ask_name(self):
+            self._send("Введите ваше настоящее имя")
+
+        def verify_name(self):
+            if core.Validator.name(self._current_request):
+                self._store("name", self._current_request)
+            else:
+                self._send("Некорректное имя. Попробуйте снова")
+                raise core.UserInputError("invalid name")
+
+        def ask_surname(self):
+            self._send("Введите вашу настоящую фамилию")
+
+        def verify_surname(self):
+            if core.Validator.surname(self._current_request):
+                self._store("surname", self._current_request)
+            else:
+                self._send("Некорректная фамилия. Попробуйте снова")
+                raise core.UserInputError("invalid surname")
+
+        def ask_password(self):
+            self._send(f"Придумайте пароль, содержащий {config.PASSWORD_LENGTH} цифры")
+
+        def verify_password(self):
+            if core.Validator.create_password(self._current_request):
+                self._store("password", self._current_request)
+                # завершение регистрации
+                record = Tables.Users(
+                    telegram_id=self._me.get_ID(),
+                    role="учитель",
+                    name=self._data.get("name"),
+                    surname=self._data.get("surname"),
+                    password=self._data.get("password"),
+                    ref=f"tg://user?id={self._me.get_ID()}"
+                )
+                Manager.write(record)
+                self._send("Вы зарегистрированы как учитель")
+                self.completed = True
+            else:
+                self._send("Некорректный пароль. Попробуйте снова")
+                raise core.UserInputError("invalid password")
+
+    class StudentRegistration(_BaseRegistration):
+        """Пошаговая регистрация ученика: имя → фамилия → пароль → город → школа → класс → сохранение."""
+        def __init__(self, ID):
+            super().__init__(ID)
+            self._chain = [
+                self.ask_name, self.verify_name,
+                self.ask_surname, self.verify_surname,
+                self.ask_password, self.verify_password,
+                self.ask_city, self.verify_city,
+                self.ask_school, self.verify_school,
+                self.ask_class, self.verify_class,
+            ]
+            self._max_i = len(self._chain) - 1
+
+        def ask_name(self):
+            self._send("Введите ваше настоящее имя")
+
+        def verify_name(self):
+            if core.Validator.name(self._current_request):
+                self._store("name", self._current_request)
+            else:
+                self._send("Некорректное имя. Попробуйте снова")
+                raise core.UserInputError("invalid name")
+
+        def ask_surname(self):
+            self._send("Введите вашу настоящую фамилию")
+
+        def verify_surname(self):
+            if core.Validator.surname(self._current_request):
+                self._store("surname", self._current_request)
+            else:
+                self._send("Некорректная фамилия. Попробуйте снова")
+                raise core.UserInputError("invalid surname")
+
+        def ask_password(self):
+            self._send(f"Придумайте пароль, содержащий {config.PASSWORD_LENGTH} цифры")
+
+        def verify_password(self):
+            if core.Validator.create_password(self._current_request):
+                self._store("password", self._current_request)
+            else:
+                self._send("Некорректный пароль. Попробуйте снова")
+                raise core.UserInputError("invalid password")
+
+        def ask_city(self):
+            self._send("В каком городе вы учитесь?")
+
+        def verify_city(self):
+            if core.Validator.city(self._current_request):
+                self._store("city", self._current_request)
+            else:
+                self._send("Некорректный город. Попробуйте снова")
+                raise core.UserInputError("invalid city")
+
+        def ask_school(self):
+            self._send("В какой школе вы учитесь?")
+
+        def verify_school(self):
+            if core.Validator.school(self._current_request):
+                self._store("school", int(self._current_request.split(" ")[-1]))
+            else:
+                self._send("Некорректный номер школы. Попробуйте снова")
+                raise core.UserInputError("invalid school")
+
+        def ask_class(self):
+            self._send("В каком классе вы учитесь? (например, 9а)")
+
+        def verify_class(self):
+            if core.Validator.class_number(self._current_request):
+                self._store("student_class", self._current_request)
+                # завершение регистрации
+                record = Tables.Users(
+                    telegram_id=self._me.get_ID(),
+                    role="ученик",
+                    name=self._data.get("name"),
+                    surname=self._data.get("surname"),
+                    password=self._data.get("password"),
+                    city=self._data.get("city"),
+                    school=self._data.get("school"),
+                    student_class=self._data.get("student_class"),
+                    ref=f"tg://user?id={self._me.get_ID()}"
+                )
+                Manager.write(record)
+                self._send("Вы зарегистрированы как ученик")
+                self.completed = True
+            else:
+                self._send("Некорректное значение класса. Попробуйте снова")
+                raise core.UserInputError("invalid class")
 
     def __registration(self, enterData, role):
         """Сохраняет данные регистрации пользователя c указанной ролью."""
@@ -540,15 +698,46 @@ class Unregistered(User):
 
     def teacher_registration(self):
         """Начинает регистрацию учителя"""
-        self.current_registration = btn.Unreg.teacher_registration()
-        self.current_registration.set_ID(self._ID)
-        self.current_registration.run()
+        # Новый процесс на базе core.Process
+        self.reg_process = self.TeacherRegistration(self._ID)
+        self.in_registration = True
+        self._current_command = self._cancelable_registration_execute
+        # немедленно вывести первый вопрос
+        self._current_command()
 
     def student_registration(self):
         """Начинает регистрацию ученика"""
-        self.current_registration = btn.Unreg.student_registration()
-        self.current_registration.set_ID(self._ID)
-        self.current_registration.run()
+        # Новый процесс на базе core.Process
+        self.reg_process = self.StudentRegistration(self._ID)
+        self.in_registration = True
+        self._current_command = self._cancelable_registration_execute
+        # немедленно вывести первый вопрос
+        self._current_command()
+        return True
+
+    def cancel_current_action(self):
+        """Очищает состояние активного действия (регистрация и т.п.)."""
+        try:
+            if self.reg_process:
+                self.reg_process.stop()
+            self.reg_process = None
+            self.in_registration = False
+        except Exception:
+            pass
+
+    @cancelable
+    def _cancelable_registration_execute(self):
+        """Выполняет один шаг регистрационного процесса с поддержкой отмены."""
+        if not self.reg_process:
+            return False
+        # передаём последний ввод в процесс
+        self.reg_process.update_last_request(self._current_request)
+        self.reg_process.execute()
+        # если процесс завершён, очищаем состояние
+        if getattr(self.reg_process, "completed", False):
+            self.reg_process = None
+            self.in_registration = False
+            self._current_command = None
         return True
 
     def _determine_role_from_data(self, data: dict) -> str:
